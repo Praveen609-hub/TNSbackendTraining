fb7713345d196fed9a1469665dac93ef6ea8b127
Praveen609-hub <ppraveen8044@gmail.com>
2025-11-12 20:55:42 +0530

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,22 @@
+public class Loops {
+    public static void main(String[] args) {
+        // Example of a for loop
+        for (int i = 0; i < 5; i++) {
+            System.out.println("For Loop Iteration: " + i);
+        }
+
+        // Example of a while loop
+        int j = 0;
+        while (j < 5) {
+            System.out.println("While Loop Iteration: " + j);
+            j++;
+        }
+
+        // Example of a do-while loop
+        int k = 0;
+        do {
+            System.out.println("Do-While Loop Iteration: " + k);
+            k++;
+        } while (k < 5);
+    }
+}
